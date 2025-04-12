e16caf4ffac7f0d1135ffff7bceda75587c079e2
Tamara Dobrenkova <tamara.dobrenkova@gmail.com>
2025-04-12 12:39:50 +0200

    homework

---
+++ homework_3/math_1.py
@@ -0,0 +1,21 @@
+# определяем переменные
+a = 2
+b = 4
+
+# выполняем арифметические действия
+sum_result = a + b
+sub_result = b - 2
+mul_result = a * b
+div_result = b / a
+
+# выводим результат
+print("Сумма:", sum_result)
+print("Разность:", sub_result)
+print("Произведение:", mul_result)
+print("Частное:", div_result)
+
+
+
+
+
+
---
+++ homework_3/maths_2.py
@@ -0,0 +1,13 @@
+# определяем переменные
+x = 20
+y = 2
+
+# выполняем арифметические действия примера x − y / 1 + xy
+div_result = y / 1
+mal_result = x * y
+sum_result = div_result + mal_result
+sub_result = x - sum_result
+
+# выводим результат
+print("Результат:", sub_result)
+

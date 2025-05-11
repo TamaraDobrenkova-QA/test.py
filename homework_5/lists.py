bb4e72626abeca1e355613bd47962a4ecaa61ef8
Tamara Dobrenkova <tamara.dobrenkova@gmail.com>
2025-05-11 12:17:27 +0200

    homework_5

--- homework_5/lists.py
+++ homework_5/lists.py
@@ -4,17 +4,38 @@ a, b, c, d, e = person
 print(a, b, c, d, e)
 
 #срез и метод index
-list = [42, 514, 9]
-a, b, c = list
-my_text = f'результат операции: {a}, результат операции: {b}, результат работы программы: {c}'
-print(my_text)
-a = a + 10
-b = b + 10
-c = c + 10
-my_text_2 = f'результат операции: {a}, результат операции: {b}, результат работы программы: {c}'
-print(my_text_2)
+text_1 = 'результат операции: 42'
+text_2 = 'результат операции: 514'
+text_3 = 'результат работы программы: 9'
+print(text_1)
+print(text_2)
+print(text_3)
 
-# распесатка списков с дополнительной информацией
+index_1 = text_1.index(":")+1
+index_2 = text_2.index(":")+1
+index_3 = text_3.index(":")+1
+num_1 = int(text_1[text_1.index(":") + 1:]) + 10
+num_2 = int(text_2[text_2.index(":") + 1:]) + 10
+num_3 = int(text_3[text_3.index(":") + 1:]) + 10
+text_1_updated = text_1[:index_1] + str(num_1)
+text_2_updated = text_2[:index_2] + str(num_2)
+text_3_updated = text_3[:index_3] + str(num_3)
+
+print(text_1_updated)
+print(text_2_updated)
+print(text_3_updated)
+
+# list = [42, 514, 9]
+# a, b, c = list
+# my_text = f'результат операции: {a}, результат операции: {b}, результат работы программы: {c}'
+# print(my_text)
+# a = a + 10
+# b = b + 10
+# c = c + 10
+# my_text_2 = f'результат операции: {a}, результат операции: {b}, результат работы программы: {c}'
+# print(my_text_2)
+
+# распечатка списков с дополнительной информацией
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 a, b, c = students
